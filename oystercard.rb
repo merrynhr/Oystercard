bd763e2dcd9451b50bb99220afed356c973ebbe9
Merryn <merrynhr@gmail.com>
2021-06-02 17:37:31 +0100

    halfway through step 12: still need to write tests

--- oystercard.rb
+++ oystercard.rb
@@ -2,11 +2,12 @@ class Oystercard
 
    MAX_BALANCE = 90
    MIN_CHARGE = 1
-   attr_reader :balance, :entry_station
+   attr_reader :balance, :entry_station, :list_of_journeys
   
    def initialize
     @balance = 0
     @entry_station = nil
+    @list_of_journeys = []
    end
     
    def top_up(amount)
@@ -22,10 +23,13 @@ class Oystercard
    def touch_in(station)
      raise 'insufficient funds' if @balance < MIN_CHARGE
      @entry_station = station
+     @list_of_journeys.push({@entry_station => ''})
     end 
 
-   def touch_out
+   def touch_out(station)
     deduct(MIN_CHARGE)
+    @exit_station = station
+    @list_of_journeys[0][@entry_station] = @exit_station
     @entry_station = nil
    end
 
